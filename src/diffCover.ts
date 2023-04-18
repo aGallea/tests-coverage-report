1d6ae42e5c3062a61ef39bb0e0022ae1d73da3e2
Asaf Gallea <asaf.gallea@dynamicyield.com>
2023-04-18 15:07:46 +0300

    chore: logs

--- src/diffCover.ts
+++ src/diffCover.ts
@@ -41,14 +41,20 @@ const getDiff = async (
   referral: DiffCoverRef,
 ): Promise<DiffInfo[]> => {
   const diffInfo: DiffInfo[] = [];
+  core.info('getDiff');
   for (const fileCoverInfo of coverageInfo[referral]) {
+    core.info(`fileCoverInfo: [${fileCoverInfo}]`);
+    core.info(`changedFiles: [${changedFiles}]`);
     for (const currFile of changedFiles) {
+      core.info(`currFile: [${currFile}]`);
       const changedLinesExec = await execCommand(
         `git blame ${currFile} | grep -n '${commitsSha.join('\\|')}' | cut -f1 -d:`,
       );
       if (changedLinesExec.status === 'success') {
         const changedLines =
           changedLinesExec.stdout?.split('\n').filter((line) => line) || [];
+        core.info(`changedLinesExec.stdout: [${changedLinesExec.stdout}]`);
+        core.info(`changedLines: [${changedLines}]`);
         if (changedLines.length) {
           if (fileCoverInfo.lines.details.length) {
             if (fileCoverInfo.file === currFile) {
